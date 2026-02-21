e7766188b8fd9325ede3a791e0df60b4c1b93f0e
santiagoBotina <santiagoboe04@gmail.com>
2026-02-20 20:33:33 -0500

    feat: implement compose ports

--- ms-transaction-evaluator/cmd/api/main.go
+++ ms-transaction-evaluator/cmd/api/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
 
 func main() {
+	godotenv.Load()
+
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
 
 	e.GET("/", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Hola, World!")
+		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	if err := e.Start(":3000"); err != nil {
+	port := os.Getenv("EVALUATOR_APP_PORT")
+
+	if err := e.Start(":" + port); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
 	}
 }
--- ms-transaction-evaluator/go.mod
+++ ms-transaction-evaluator/go.mod
@@ -2,6 +2,9 @@ module ms-transaction-evaluator
 
 go 1.25.0
 
-require github.com/labstack/echo/v5 v5.0.4
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v5 v5.0.4
+)
 
 require golang.org/x/time v0.14.0 // indirect
